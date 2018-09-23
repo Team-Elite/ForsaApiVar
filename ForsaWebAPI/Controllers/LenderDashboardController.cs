247bc58aaa905405728912de7de4332549543a6b
ishucityinformat <ishucityinformat>
2018-09-23 23:22:22 +0530

    Worked on View All Prices

--- ForsaWebAPI/Controllers/LenderDashboardController.cs
+++ ForsaWebAPI/Controllers/LenderDashboardController.cs
@@ -23,5 +23,43 @@ namespace ForsaWebAPI.Controllers
             }
             return Json(new { IsSuccess = true, data = HelperClass.DataTableToJSONWithJavaScriptSerializer(dt) });
         }
+
+        [HttpGet]
+        public IHttpActionResult DeselectBank(int userId, int bankId, Boolean IsSelected)
+        {
+            SqlParameter[] param = new SqlParameter[3];
+            param[0] = new SqlParameter("@UserId", userId);
+            param[1] = new SqlParameter("@BankId", bankId);
+            param[2] = new SqlParameter("@IsSelected", IsSelected);
+            SqlHelper.ExecuteScalar(HelperClass.ConnectionString, "USP_DeselectBank ", System.Data.CommandType.StoredProcedure, param);
+            return Json(new { IsSuccess = true });
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAllBanksWithStatusIsDeselected(int userId)
+        {
+            SqlParameter[] param = new SqlParameter[1];
+            param[0] = new SqlParameter("@UserId", userId);
+            var dt = SqlHelper.ExecuteDataTable(HelperClass.ConnectionString, "USP_Lender_GetAllBanksWithStatusIsDeselected", System.Data.CommandType.StoredProcedure, param);
+            if (dt == null || dt.Rows.Count == 0)
+            {
+                return Json(new { IsSuccess = false });
+            }
+            return Json(new { IsSuccess = true, data = HelperClass.DataTableToJSONWithJavaScriptSerializer(dt) });
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAllBanksWithInterestRateHorizontalyWhichAreNotDeSelected(int userId)
+        {
+            SqlParameter[] param = new SqlParameter[1];
+            param[0] = new SqlParameter("@UserId", userId);
+            var dt = SqlHelper.ExecuteDataTable(HelperClass.ConnectionString, "USP_GetAllBanksWithInterestRateHorizontalyWhichAreNotDeSelected", System.Data.CommandType.StoredProcedure, param);
+            if (dt == null || dt.Rows.Count == 0)
+            {
+                return Json(new { IsSuccess = false });
+            }
+            return Json(new { IsSuccess = true, data = HelperClass.DataTableToJSONWithJavaScriptSerializer(dt) });
+        }
+
     }
 }
